b2ac33a17ecfa4b7686316d70b8675fec68e7f8b
luisdlpr <reyesluichi@hotmail.com>
2023-09-05 17:31:25 +1000

    use client on all pages, not able to build for some reason seeing if this fixes

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import PuzzleUI from "./components/PuzzleUI";
 import Login from "./components/Login";
@@ -5,23 +7,39 @@ import { Bungee } from "next/font/google";
 
 const bungee = Bungee({ subsets: ["latin"], weight: "400" });
 
-const getPuzzle = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/play`, {
-    method: "GET",
-    cache: "no-store",
-  });
+// const getPuzzle = async () => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/play`, {
+//     method: "GET",
+//     cache: "no-store",
+//   });
+//
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//
+//   const puzzleJSON = await res.json();
+//
+//   return { puzzleJSON };
+// };
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+export default function Home() {
+  const [puzzle, setPuzzle] = React.useState<{}>();
 
-  const puzzleJSON = await res.json();
-
-  return { puzzleJSON };
-};
-
-export default async function Home() {
-  const { puzzleJSON } = await getPuzzle();
+  React.useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/play`, {
+      method: "GET",
+      cache: "no-store",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return res.json();
+      })
+      .then((json) => {
+        setPuzzle(json);
+      });
+  }, []);
 
   return (
     <main className="text-center flex flex-col items-center">
@@ -29,7 +47,7 @@ export default async function Home() {
         <h1 className={`${bungee.className} text-4xl m-4`}>Reed Riddle 🎬</h1>
         <Login />
       </div>
-      <PuzzleUI puzzleJSON={puzzleJSON} />
+      {puzzle != undefined && <PuzzleUI puzzleJSON={puzzle} />}
     </main>
   );
 }
