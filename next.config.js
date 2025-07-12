9d53e94ae951afa2ed3cc5a0ab7c048592223002
cunky <cunky@luisdlpr.com>
2025-07-12 08:08:05 +0000

    config change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_URL ? new URL(process.env.NEXT_PUBLIC_BASE_URL).pathname : '',
+    async redirects() {
+        return ([
+            {
+                source: '/',
+                destination: '/myapp',
+                permanent: true,
+                basePath: false,
+            },
+        ]);
+    },
     images: {
         domains: ["image.tmdb.org"],
         remotePatterns: [
