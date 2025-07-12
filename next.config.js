4959481385f097e9489eb58d98ba9c55228b8c41
Luis Reyes <luis@Luiss-MBP.gateway>
2025-07-12 18:09:21 +1000

    update basepath to work locally

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NEXT_PUBLIC_BASE_URL ? new URL(process.env.NEXT_PUBLIC_BASE_URL).pathname : '',
+    basePath: process.env.NEXT_PUBLIC_BASE_URL ? 
+        (new URL(process.env.NEXT_PUBLIC_BASE_URL).pathname === '/' ? '' : new URL(process.env.NEXT_PUBLIC_BASE_URL).pathname) : '',
     images: {
         domains: ["image.tmdb.org"],
         remotePatterns: [
