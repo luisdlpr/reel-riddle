86e7d70d86f5d596943a7c99a0cdabfc9272cca6
cunky <cunky@luisdlpr.com>
2025-07-12 07:36:15 +0000

    use basepath when hosting

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        port: '',
-        pathname: '/t/p/**',
-      },
-    ],
-  },
+    basePath: process.env.NEXT_PUBLIC_BASE_URL ? new URL(process.env.NEXT_PUBLIC_BASE_URL).pathname : '',
+    images: {
+        domains: ["image.tmdb.org"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "image.tmdb.org",
+                port: "",
+                pathname: "/t/p/**",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
